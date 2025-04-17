25f48a64f04050c7c3d0a2088a4425461759cd5e
Martin Simunek <public@e-mis.cz>
2025-04-17 20:01:55 +0200

    Výpis dodatečné hlášky + komentáře
    Úvodní seznámení s třídou Main a metodou main().

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+    /**
+     * Hlavní metoda programu. Dopíši tam něco navíc.
+     * @param args Argumenty příkazového řádku.
+     */
     public static void main(String[] args) {
+
+
         System.out.println("Hello, World!");
+        System.out.println("Toto je můj první příkaz.");
+        // GitHub Copilot jůlfijliasjůilgjaliůgjalůigjaliůjgailjgalij gliarj
+        // Komentář - řádkový
+
+        /*
+         * Komentář - blokový
+         * Když potřebuji napsat komentář s více řádky.
+         */
+
+
     }
 }
